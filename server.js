950ccfd173713d023edd2bc91b850cf70b786c02
Jai Shukla <jay@live.co.in>
2019-12-09 12:31:19 +0530

    server ready

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 
 
-app.use(express.static(__dirname + '../dist/ProDex-Frontend/'));
+app.use(express.static(__dirname + '/dist/ProDex-Frontend/'));
 app.listen(8080);
 console.log("App listening on port 8080");
