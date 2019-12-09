290114845b563b299256f96e9511e4570d638631
Jai Shukla <jay@live.co.in>
2019-12-09 12:47:51 +0530

    serverPortadded

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
-
+var PORT = process.env.PORT || 5000
 
 app.use(express.static(__dirname + '/dist/ProDex-Frontend/'));
-app.listen(8080);
-console.log("App listening on port 8080");
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
